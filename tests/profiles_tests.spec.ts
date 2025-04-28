198571dfd72fa00f6d2a6417992a994831c6fa78
curtis-taylor <curtis.t@gmail.com>
2025-04-27 23:14:44 -0400

    Started on Teams API tests. swagger ui working better

--- tests/profiles_tests.spec.ts
+++ tests/profiles_tests.spec.ts
@@ -3,11 +3,11 @@ import {test, expect } from "@playwright/test";
 
 
 test('API GET Request', async({request}) => {
-    const response = await request.get('http://127.0.0.1:8787/profiles');
+    const response = await request.get('http://localhost:4242/profiles');
 
     expect(response.status()).toBe(200);
 
     const text = await response.text();
+    console.log(response.text)
     expect(text).toContain('John Doe');
 })
-
